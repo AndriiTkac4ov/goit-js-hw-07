dadc3bac298c3251671f2456e07619450a51772c
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-10-17 22:54:24 +0300

    start task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryListEl = document.querySelector('.gallery');
+
+const createGalleryMarkup = function (array) {
+    return array
+        .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `
+        })
+        .join('');
+};
+
+const galleryMarup = createGalleryMarkup(galleryItems);
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryMarup);
+
 console.log(galleryItems);
